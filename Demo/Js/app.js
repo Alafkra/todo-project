dca0f8c418023d645fa90c0ccc088c0b5a6faf33
Zaid Saeed <Zaidayasrah1@gmail.com>
2022-01-25 02:03:19 +0200

    Task5 Updated

--- Demo/Js/app.js
+++ Demo/Js/app.js
@@ -1,1 +1,37 @@
-alert("Hello")
+'use strict';
+
+var input0 = prompt(' Enter Your Name: ');
+console.log('Your Name is : ', input0);
+
+var input1 = prompt(' Enter Your gender: ');
+console.log('Your gender is : ', input1);
+var gender = ('male' , 'female');
+
+var input3 = prompt(' Enter Your age: ');
+
+if (input3 >=0) {
+
+    console.log('Your age is : ', input3);
+    
+} else {
+    
+    alert('your age less than 0')
+    
+}
+
+var result = confirm('Do you want to skip the Welcoming Message');
+console.log('Confirm result is : ', result);
+
+
+if (input1=male) {
+
+    console.log('Mr '), input1 ;
+
+}else if (input1=='female'){
+  
+  console.log('Ms ',input1);
+
+}else{
+    alert ('Welcom')
+}
+
